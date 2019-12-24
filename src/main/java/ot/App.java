ef9fdfea9a9423724a0258a22827249002fad822
oota <oota@mail>
2019-12-24 16:32:28 +0900

    udpate

--- src/main/java/ot/App.java
+++ src/main/java/ot/App.java
@@ -5,7 +5,7 @@ package ot;
 
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "Hello world. 0";
     }
 
     public static void main(String[] args) {
